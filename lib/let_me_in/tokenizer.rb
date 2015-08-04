7b32d18b282dacddf23ef6949d765d76ba534c5e
monika <monikaglier@gmail.com>
2015-08-04 17:29:11 +0200

    added spec for module Tokenizer

--- lib/let_me_in/tokenizer.rb
+++ lib/let_me_in/tokenizer.rb
@@ -1,16 +1,21 @@
+require 'base64'
+
 module LetMeIn
   module Tokenizer
-    require 'base64'
+    DELIMITER = '.'.freeze
 
     def self.generate_token
-      Base64.encode64("#{SecureRandom.hex(10)}.#{Time.now}")
+      Base64.encode64 [SecureRandom.hex(12), Time.now.xmlschema].join(DELIMITER)
+    end
+
+    def self.decode_token(token)
+      Base64.decode64(token).split(DELIMITER)
     end
 
     def self.decode_time(token)
-      token_time = Base64.decode64(token).slice(-25,25)
+      token_time = decode_token(token)[1]
       Time.parse(token_time)
     end
-
   end
 end
 
