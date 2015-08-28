171c82270f582c7d0b8a1e807490321152adbb84
Bernard Potocki <bernard.potocki@imanel.org>
2015-08-28 20:02:09 +0200

    Add more tests for tokenizer, including ivalid ones but passing current code

--- spec/dummy/spec/modules/tokenizer_spec.rb
+++ spec/dummy/spec/modules/tokenizer_spec.rb
@@ -1,19 +1,37 @@
 require_relative '../spec_helper'
 
 describe JumpIn::Tokenizer do
+  let(:random) { 'e3d6ce35f9f6f7ea696cd180' }
+  let(:time)   { Time.parse('2015-08-28T19:35:56+02:00') }
+  let(:token)  { "ZTNkNmNlMzVmOWY2ZjdlYTY5NmNkMTgwLjIwMTUtMDgtMjhUMTk6MzU6NTYr\nMDI6MDA=\n" }
 
   context ".generate_token" do
     it 'generates token' do
-      token = JumpIn::Tokenizer.generate_token
-      expect(token).to_not eq(nil)
+      allow(SecureRandom).to receive(:hex).with(12).and_return(random)
+      generated_token = nil
+      travel_to time do
+        generated_token = JumpIn::Tokenizer.generate_token
+      end
+      expect(generated_token).to eq(token)
+    end
+  end
+
+  context ".decode_and_split_token" do
+    it "decodes valid token" do
+      expect(JumpIn::Tokenizer.decode_and_split_token(token)).to eq([random, time.xmlschema])
+    end
+    it "return gibberishi on invalid token" do
+      return_value = ["\x8A{\xDA\x96'"].map {|v| v.force_encoding('ASCII-8BIT') }
+      expect(JumpIn::Tokenizer.decode_and_split_token("invalid")).to eq(return_value)
     end
   end
 
   context ".decode_time" do
-    it "decodes time correctly" do
-      time = Time.now.xmlschema
-      token = Base64.encode64 [SecureRandom.hex(12), time].join(JumpIn::Tokenizer::DELIMITER)
+    it "decodes time for valid token" do
       expect(JumpIn::Tokenizer.decode_time(token)).to eq(time)
+    end
+    it "raises error for invalid token" do
+      expect { JumpIn::Tokenizer.decode_time("invalid") }.to raise_error(TypeError, "no implicit conversion of nil into String")
     end
   end
 end
